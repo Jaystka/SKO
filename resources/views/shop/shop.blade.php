763dedfc062f189330dd6cdd08136de78dec1646
Luthfa Sobrian <luthfa.s.p@gmail.com>
2024-06-04 21:00:05 +0700

    add tombol-arrow

--- resources/views/shop/shop.blade.php
+++ resources/views/shop/shop.blade.php
@@ -6,9 +6,10 @@
 @endsection
 
 @section('content')
-<main class="container-sm px-10 py-24 font-MadeTomy-Medium">
+<main class="container px-10 py-24 font-MadeTomy-Medium">
     <section class="mb-16">
         <h2 class="text-2xl font-semibold">Sepatu Compass</h2>
+        <div class="flex">
         <div class="owl-carousel flex flex-col md:flex-row justify-between gap-2">
             <div class="text-center">
                 <a href="product">
@@ -59,46 +60,72 @@
                     <p class="text-red-600">IDR 578.000</p>
                 </a>
             </div>
-            <div class="text-center">
-                <a href="product">
-                    <img src="dist/img/Cmp2.png" alt="Proto Lite Purple" class="mx-auto">
-                    <h3 class="mt-4">Proto Lite Purple</h3>
-                    <p class="text-red-600">IDR 578.000</p>
-                </a>
-            </div>
-            <div class="text-center">
-                <a href="product">
-                    <img src="dist/img/Cmp2.png" alt="Proto Lite Purple" class="mx-auto">
-                    <h3 class="mt-4">Proto Lite Purple</h3>
-                    <p class="text-red-600">IDR 578.000</p>
-                </a>
-            </div>
-            <div class="text-center">
-                <a href="product">
-                    <img src="dist/img/Cmp2.png" alt="Proto Lite Purple" class="mx-auto">
-                    <h3 class="mt-4">Proto Lite Purple</h3>
-                    <p class="text-red-600">IDR 578.000</p>
+        </div>
+        <div class="flex justify-end">
+                <a href="next-page-url" class="page-arrow flex items-center justify-center w-7 h-7 bg-transparent border border-black  rounded-full">
+                    <img src="dist/img/right-arrow.png" alt="Arrow-Compas" class="w-3 h-3">
                 </a>
             </div>
         </div>
     </section>
     <section>
         <h2 class="text-2xl font-semibold mt-20">Sepatu Ventela</h2>
-        <div class="flex flex-col md:flex-row justify-between gap-2">
-            <div class="text-center">
-                <img src="img/Ven1.png" alt="Public Low Black Natural" class="mx-auto">
-                <h3 class="mt-4">Public Low Black Natural</h3>
-                <p class="text-red-600">IDR 229.000</p>
+        <div class="flex">
+        <div class="owl-carousel flex flex-col md:flex-row justify-between gap-2">
+        <div class="text-center">
+                <a href="product">
+                    <img src="dist/img/Ven1.png" alt="Proto Lite Purple" class="mx-auto">
+                    <h3 class="mt-4">Proto Lite Purple</h3>
+                    <p class="text-red-600">IDR 578.000</p>
+                </a>
             </div>
             <div class="text-center">
-                <img src="img/Ven2.png" alt="Public Low Grey" class="mx-auto">
-                <h3 class="mt-4">Public Low Grey</h3>
-                <p class="text-red-600">IDR 239.000</p>
+                <a href="product">
+                    <img src="dist/img/Ven2.png" alt="Proto Lite Purple" class="mx-auto">
+                    <h3 class="mt-4">Proto Lite Purple</h3>
+                    <p class="text-red-600">IDR 578.000</p>
+                </a>
             </div>
             <div class="text-center">
-                <img src="img/Ven3.png" alt="Public Low Yellow" class="mx-auto">
-                <h3 class="mt-4">Public Low Yellow</h3>
-                <p class="text-red-600">IDR 239.000</p>
+                <a href="product">
+                    <img src="dist/img/Ven2.png" alt="Proto Lite Purple" class="mx-auto">
+                    <h3 class="mt-4">Proto Lite Purple</h3>
+                    <p class="text-red-600">IDR 578.000</p>
+                </a>
+            </div>
+            <div class="text-center">
+                <a href="product">
+                    <img src="dist/img/Ven2.png" alt="Proto Lite Purple" class="mx-auto">
+                    <h3 class="mt-4">Proto Lite Purple</h3>
+                    <p class="text-red-600">IDR 578.000</p>
+                </a>
+            </div>
+            <div class="text-center">
+                <a href="product">
+                    <img src="dist/img/Ven2.png" alt="Proto Lite Purple" class="mx-auto">
+                    <h3 class="mt-4">Proto Lite Purple</h3>
+                    <p class="text-red-600">IDR 578.000</p>
+                </a>
+            </div>
+            <div class="text-center">
+                <a href="product">
+                    <img src="dist/img/Ven2.png" alt="Proto Lite Purple" class="mx-auto">
+                    <h3 class="mt-4">Proto Lite Purple</h3>
+                    <p class="text-red-600">IDR 578.000</p>
+                </a>
+            </div>
+            <div class="text-center">
+                <a href="product">
+                    <img src="dist/img/Ven2.png" alt="Proto Lite Purple" class="mx-auto">
+                    <h3 class="mt-4">Proto Lite Purple</h3>
+                    <p class="text-red-600">IDR 578.000</p>
+                </a>
+            </div>
+        </div>
+        <div class="flex justify-end">
+                <a href="next-page-url" class="page-arrow flex items-center justify-center w-7 h-7 bg-transparent border border-black  rounded-full hover:bg-slate-400">
+                    <img src="dist/img/right-arrow.png" alt="Arrow-Compas" class="w-3 h-3">
+                </a>
             </div>
         </div>
     </section>
@@ -115,16 +142,20 @@
     $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
+    nav:false,
+    responsiveClass:true,
     responsive:{
     0:{
-    items:1
+    items:1,
+    loop:false
     },
     600:{
-    items:2
+    items:2,
+    loop:false
     },
     1000:{
-    items:3
+    items:3,
+    loop:false
     }
     }
     })
