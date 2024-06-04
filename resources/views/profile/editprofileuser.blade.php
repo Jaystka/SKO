01bbc84d7f068e393ecc74218ab5151bd2ebc206
jaystka <jaystka@students.amikom.ac.id>
2024-06-04 17:26:17 +0700

    Shop Carousel

--- resources/views/profile/editprofileuser.blade.php
+++ resources/views/profile/editprofileuser.blade.php
@@ -55,3 +55,12 @@
   </div>
 </section>
 @endsection
+
+@section('script')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script>
+
+</script>
+@endsection
